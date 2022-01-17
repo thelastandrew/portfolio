73ccd9212d221e8e5dbc6c78d3c06de989502ff0
thelastandrew <thelastandrew@gmail.com>
2022-01-17 06:54:03 +0300

    feat: add showing nav-menu when click on burger icon

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,5 +1,7 @@
 const burgerMenu = document.querySelector(".burger-menu");
+const nav = document.querySelector(".nav");
 
 burgerMenu.addEventListener("click", function (e) {
   burgerMenu.classList.toggle("active");
+  nav.classList.toggle("show");
 });
