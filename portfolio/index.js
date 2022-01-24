8db99f95832168837b4812185a98d3e1ac488831
thelastandrew <thelastandrew@gmail.com>
2022-01-24 08:16:17 +0300

    feat: improve burger menu functionality

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,7 +1,18 @@
 const burgerMenu = document.querySelector(".burger-menu");
 const nav = document.querySelector(".nav");
+const navItems = document.querySelectorAll(".nav-item");
+const body = document.querySelector(".body");
 
 burgerMenu.addEventListener("click", function (e) {
   burgerMenu.classList.toggle("active");
+  body.classList.toggle("block");
   nav.classList.toggle("show");
+});
+
+navItems.forEach((element) => {
+  element.addEventListener("click", function (e) {
+    burgerMenu.classList.toggle("active");
+    body.classList.toggle("block");
+    nav.classList.toggle("show");
+  });
 });
